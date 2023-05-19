2a1d5a30ad7aaae38bb5d98b50323a4d145e21bb
Matthew Zou <mzou1212@gmail.com>
2023-05-19 17:33:25 -0400

    update

--- cmds/music.py
+++ cmds/music.py
@@ -273,7 +273,7 @@ class Music(commands.Cog):
         search: str [Required]
             The song to search and retrieve using YTDL. This could be a simple search, an ID or URL.
         """
-        await ctx.trigger_typing()
+        await ctx.typing()
 
         vc = ctx.voice_client
 
