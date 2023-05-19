a70dc2d01b3e9c3d6ece9489be909b416c6ef767
Matthew Zou <mzou1212@gmail.com>
2023-05-18 23:25:58 -0400

    update

--- cmds/chatGPT.py
+++ cmds/chatGPT.py
@@ -1,15 +1,28 @@
-import discord
+import discord, os
 from discord.ext import commands
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+
+# Load environment variables
+ENABLED = False
 
 class ChatGPT(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def is_enabled(self, ctx, function):
+        if ENABLED:
+            function()
+        else:
+            ctx.send("This feature is currently disabled.")
+
     # Say something
     @commands.command(name='say')
     async def say(self, ctx, *, message):
         """ Say something """
-        await ctx.send(message)
+        await self.is_enabled(ctx.send(message))
+        
 
 async def setup(bot):
     await bot.add_cog(ChatGPT(bot))
