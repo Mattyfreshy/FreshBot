83e408a775a3499975b1703de72c8c14df60c170
Matthew Zou <mzou1212@gmail.com>
2023-06-05 00:51:21 -0400

    update

--- cogs/music_slash.py
+++ cogs/music_slash.py
@@ -437,9 +437,10 @@ class Music(commands.Cog):
         if not queue.empty():
             requester = YTDLSource.discord_requester(interaction=interaction)
             await interaction.response.send_message(f'**{requester}**: Skipped {count} song(s)!')
-            await queue.get() # Sometimes removes a song from the queue
-            # for _ in range(count):
-            #     queue.task_done()
+            # await queue.get() # Sometimes removes a song from the queue
+            queue.task_done()
+            for _ in range(count):
+                await queue.get()
         else:
             await interaction.response.send_message('There is no next song on the waiting list.')
 
